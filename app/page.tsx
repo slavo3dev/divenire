f0b8348543ed2c0eeb6b6435d57ae214db9e04c4
slavo3dev <slavo@slavo.io>
2025-01-15 19:25:53 -0500

    adding font to the app

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
     return (
-    <div>
-        <h1>Divenir</h1>
-        <Button>Click me</Button>
-    </div>
+        <div>
+            <h1>Divenir</h1>
+            <p>Unleash your potential</p>
+            <Button>Click me</Button>
+        </div>
   );
 }
